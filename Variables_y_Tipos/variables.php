775ff85f8db89a4583c8a5eed5f23c49ae3f44fd
Misael <misaelcudek@gmail.com>
2021-03-28 13:23:06 -0300

    Create variables.php
    Crear Variables en PHP

---
+++ Variables_y_Tipos/variables.php
@@ -0,0 +1,25 @@
+<?php
+/*En PHP las variables se representan con un signo de dólar seguido por el nombre de la variable. El nombre de la variable es sensible a minúsculas y mayúsculas.
+
+Los nombres de variables siguen las mismas reglas que otras etiquetas en PHP. Un nombre de variable válido tiene que empezar con una letra o un carácter de subrayado (underscore), seguido de cualquier número de letras, números y caracteres de subrayado.
+
+*/
+
+
+$texto = "Variables de texto, el valor va entre comillas!";
+$numeroentero = 99;
+$numeroconcoma = 3.141516;
+$presente= true;
+
+echo ($texto."\n");
+
+
+echo ($numeroentero."\n");
+
+
+echo ($numeroconcoma."\n");
+
+
+echo ($presente."\n");
+
+?>
