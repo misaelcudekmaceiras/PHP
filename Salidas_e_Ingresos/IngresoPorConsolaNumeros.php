9d03d4ba657e751cd5a9f20ed8558d07ebd454af
Misael <misaelcudek@gmail.com>
2021-03-28 14:01:47 -0300

    Add files via upload

---
+++ Salidas_e_Ingresos/IngresoPorConsolaNumeros.php
@@ -0,0 +1,22 @@
+<?php
+
+$num = 0;
+$resu = 0;
+
+$num=readline("Ingrese un numero: "); 
+
+echo ("El numero ingresado fue: ".$num."\n");
+
+$resu=$num+1;
+echo ("El siguiente es: ".$resu."\n");
+
+$resu=$num-1;
+echo ("El anterior es: ".$resu."\n");
+
+$resu=$num*2;
+echo ("El doble es: ".$resu."\n");
+
+$resu=$num/2;
+echo ("La mitad es: ".$resu."\n");
+
+?>
