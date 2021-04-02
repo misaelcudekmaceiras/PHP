906c6e69163fa5e4604d3601fe2d6de7b9aab69b
Misael <misaelcudek@gmail.com>
2021-04-01 22:25:27 -0300

    Add files via upload

---
+++ OperacionesAritmeticas/oparimmodulo.php
@@ -0,0 +1,16 @@
+<?php
+$numero=10;
+
+$resultadomodulo;
+
+$resultadomodulo=$numero%2;
+
+echo ("EL resto de la division de 10 por 2 es :".$resultadomodulo."\n");
+
+$numero=11;
+$resultadomodulo=$numero%2;
+
+echo ("EL resto de la division  11 por 2 es :".$resultadomodulo."\n");
+
+
+?>
