a9f21990702d9af2d3082e8d7813e2cb17e97a27
Misael <misaelcudek@gmail.com>
2021-04-01 22:17:56 -0300

    Add files via upload

---
+++ OperacionesAritmeticas/oparim.php
@@ -0,0 +1,18 @@
+<?php
+$num1=20;
+$num2=2;
+
+$resultado=0;
+
+$resultado=$num1+$num2;
+echo ("La suma es ".$resultado."\n");
+$resultado=$num1-$num2;
+echo ("La resta es ".$resultado."\n");
+$resultado=$num1/$num2;
+echo ("La division es ".$resultado."\n");
+$resultado=$num1*$num2;
+echo ("La multiplicacion ".$resultado."\n");
+
+
+
+?>
---
+++ OperacionesAritmeticas/oparimatajos.php
@@ -0,0 +1,35 @@
+<?php
+$numero=10;
+echo ("Valor inicial :".$numero."\n");
+
+$numero++;//$numero = $numero+1;
+echo ("Post incremento en 1 :".$numero."\n"."\n");
+
+$numero=10;
+echo ("Valor inicial :".$numero."\n");
+$numero--;//$numero = $numero-1;
+echo ("Post decremento en 1 :".$numero."\n"."\n");
+
+$numero=10;
+echo ("Valor inicial :".$numero."\n");
+++$numero;//$numero = $numero+1;
+echo ("Pre incremento en 1 :".$numero."\n"."\n");
+
+$numero=10;
+echo ("Valor inicial :".$numero."\n");
+--$numero;//$numero = $numero+1;
+echo ("Pre decremento en 1 :".$numero."\n"."\n");
+
+$numero=10;
+echo ("Valor inicial :".$numero."\n");
+$numero+=3;//$numero=$numero+1
+echo ("Suma 3 breve :".$numero."\n"."\n");
+
+$numero=10;
+echo ("Valor inicial :".$numero."\n");
+$numero-=2;//$numero=$numero-1
+echo ("Resta 2 breve :".$numero."\n"."\n");
+
+
+
+?>
